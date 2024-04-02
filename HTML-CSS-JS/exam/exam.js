6e90c206762a04748fd2080e24da848e950e6a2e
tuananh21 <chutuananh.vnu@gmail.com>
2024-04-02 19:10:01 +0700

    update

--- HTML-CSS-JS/exam/exam.js
+++ HTML-CSS-JS/exam/exam.js
@@ -1,30 +1,58 @@
-function validateName() {
-	var name = document.getElementsByName("name")[0].value;
-	if (name.length <= 3)
-		return "Name should be at least three characters.\n";	
-	return "";
+function validateForm() {
+    let name = document.getElementById("username").value;
+    let email = document.getElementById("email-address").value;
+    let phone = document.getElementById("phone-number").value;
+    let password = document.getElementById("password").value;
+    let retype = document.getElementById("retype").value;
+
+    if (name == "") {
+        let nameError = document.getElementById("nameError");
+        nameError.innerText = "Vui long nhap vao ten cua ban";
+        return false;
+    } else if (email.indexOf('@') == -1) {
+		let emailError = document.getElementById("emailError");
+        emailError.innerText = "Vui long nhap dung dia chi email!";
+        return false;
+	} else  if (phone == "") {
+        let phoneError = document.getElementById("phoneError");
+        phoneError.innerText = "Vui long nhap vao SDT cua ban";
+        return false;
+    } else if (password == "") {
+        let passwordError = document.getElementById("passwordError");
+        passwordError.innerText = "Vui long nhap vao mat khau";
+        return false;
+    } else if ( password != retype) {
+        let retypeError = document.getElementById("retypeError");
+        retypeError.innerText = "Mat khau khong trung khop";
+        return false;
+    } else {
+        return true;
+    }
 }
 
-function validatePassword() {
-	var password = valueOf("password");
-	var retype = valueOf("retype_password");
-	
-	if (password.length <= 6) 
-		return "Password should be at least 6 characters.\n";
-	
-	if (password != retype) 
-		return "Passwords do not match.\n";	
-	return "";
-}
+function removeError() {
+    let name = document.getElementById("username").value;
+    let email = document.getElementById("email-address").value;
+    let phone = document.getElementById("phone-number").value;
+    let password = document.getElementById("password").value;
 
-function validateEmail() {
-	var email = valueOf("email");
-	var retype = valueOf("retype_email");
-	
-	if (email.indexOf('@') == -1) 
-		return "Email should be a valid address.\n";
-	
-	if (email != retype)
-		return "Email addresses do not match.\n";
-	return "";	
+    if (name != "") {
+        let nameError = document.getElementById("nameError");
+        nameError.innerText = "";
+    }
+
+    if (email.indexOf('@') == 0){
+        let emailError = document.getElementById("emailError");
+        emailError.innerText = "";
+    }
+
+    if (phone != "") {
+        let phoneErrorError = document.getElementById("phoneError");
+        phoneErrorError.innerText = "";
+    }
+
+    if (password != "") {
+        let passwordError = document.getElementById("passwordError");
+        passwordError.innerText = "";
+    }
 }
